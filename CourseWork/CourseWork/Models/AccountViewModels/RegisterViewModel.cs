dba4f8affe511b6a221e9d10e458bd6a286fd4fe
Yury Struchkou <yuriystr98@gmail.com>
2017-08-31 11:44:21 +0300

    Adding name and surname to registration

--- CourseWork/CourseWork/Models/AccountViewModels/RegisterViewModel.cs
+++ CourseWork/CourseWork/Models/AccountViewModels/RegisterViewModel.cs
@@ -9,6 +9,14 @@ namespace CourseWork.Models.AccountViewModels
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
