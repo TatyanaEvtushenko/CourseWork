284c4a5b1b0d59d41fd81adfacbd38916560e219
Yury Struchkou <yuriystr98@gmail.com>
2017-08-31 14:21:50 +0300

    System restructuring

---
+++ CourseWork/CourseWorkDataLayer/Repositories/Implementations/ProjectRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CourseWorkDataLayer.Repositories;
+
+namespace CourseWork.DataLayer.Repositories.Implementations
+{
+    public class ProjectRepository : IRepository<Object>
+    {
+        public object AddItem(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
