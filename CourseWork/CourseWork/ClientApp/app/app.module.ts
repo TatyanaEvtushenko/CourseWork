8f6391e45c7cd0ce918f23054f4c5c06bf8ebd2f
tanya <t.evtushenko.s@yandex.by>
2017-09-02 13:56:12 +0300

    Adding AdminPage component

--- CourseWork/CourseWork/ClientApp/app/app.module.ts
+++ CourseWork/CourseWork/ClientApp/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppComponent } from './components/app/app.component';
 import { HomePageComponent } from './components/homepage/homepage.component';
 import { LoginNavComponent } from "./components/loginnav/loginnav.component";
 import { UserProjectsComponent } from './components/userprojects/userprojects.component';
+import { AdminPageComponent } from './components/adminpage/adminpage.component';
 
 import { AppService } from "./services/app.service";
 
 const appRoutes: Routes = [
     { path: '', component: HomePageComponent },
     { path: 'UserProjects', component: UserProjectsComponent },
+    { path: 'AdminPage', component: AdminPageComponent },
 ];
 
 @NgModule({
@@ -29,6 +31,7 @@ const appRoutes: Routes = [
         HomePageComponent,
         LoginNavComponent,
         UserProjectsComponent,
+        AdminPageComponent,
     ],
     providers:[
         AppService,
---
+++ CourseWork/CourseWork/ClientApp/app/components/adminpage/adminpage.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'adminpage',
+    templateUrl: './adminpage.component.html'
+})
+export class AdminPageComponent {
+}
