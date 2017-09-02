70c358607b427eda5fdb65e13b89f276827bb39d
tanya <t.evtushenko.s@yandex.by>
2017-09-02 20:34:27 +0300

    Adding title and other changes

--- CourseWork/CourseWork/ClientApp/app/components/adminpage/adminpage.component.ts
+++ CourseWork/CourseWork/ClientApp/app/components/adminpage/adminpage.component.ts
@@ -1,8 +1,12 @@
 ﻿import { Component } from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
     selector: 'adminpage',
     templateUrl: './adminpage.component.html'
 })
 export class AdminPageComponent {
+    constructor(title: Title) {
+        title.setTitle("Admin page");
+    }
 }
--- CourseWork/CourseWork/ClientApp/app/components/homepage/homepage.component.ts
+++ CourseWork/CourseWork/ClientApp/app/components/homepage/homepage.component.ts
@@ -1,8 +1,12 @@
 ﻿import { Component } from '@angular/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
     selector: 'homepage',
     templateUrl: './homepage.component.html'
 })
 export class HomePageComponent {
+    constructor(title: Title) {
+        title.setTitle("Home page");
+    }
 }
