bca1312b0d1c33b9082cd59be51c0adbc591f744
tanya <t.evtushenko.s@yandex.by>
2017-09-05 14:13:14 +0300

    Adding ErrorText component

---
+++ CourseWork/CourseWork/ClientApp/app/components/errortext/errortext.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'errortext',
+    templateUrl: './errortext.component.html',
+})
+export class ErrorTextComponent {
+    @Input() text: string;
+}
