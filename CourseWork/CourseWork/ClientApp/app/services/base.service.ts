138ca11ade7659842d4ee0be25bf2817056651af
Yury Struchkou <yuriystr98@gmail.com>
2017-09-06 09:13:28 +0300

    Fixing ugly account confirmation form

--- CourseWork/CourseWork/ClientApp/app/services/base.service.ts
+++ CourseWork/CourseWork/ClientApp/app/services/base.service.ts
@@ -11,7 +11,6 @@ export class BaseService {
         const body = JSON.stringify(argument);
         const headers = new Headers({ 'Content-Type': 'application/json' });
         const options = new RequestOptions({ headers: headers });
-        console.log(body);
         return this.http.post(path, body, options).map(this.getData).catch(this.throwError);
     }
 
